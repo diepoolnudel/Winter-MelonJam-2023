e965fd006c9e723e2d16dc1dd981d58c344bbe9e
jf6071s <jan.friedrich@alumni.fh-aachen.de>
2023-12-28 15:54:14 +0100

    add pedestle

--- Winter-MelonJam-2023/Assets/Door.cs
+++ Winter-MelonJam-2023/Assets/Door.cs
@@ -8,14 +8,23 @@ public class Door : MonoBehaviour
     [SerializeField] private float offset;
     [SerializeField] private Transform door;
 
-
+    private bool open = false;
     public void Open()
     {
+        if (open)
+            return;
+
         door.position = door.position + Vector3.up * offset;
+        open = true;
     }
 
     public void Close()
     {
+        if(!open)
+            return;
+
         door.position = door.position - Vector3.up * offset;
+
+        open = false;
     }
 }
---
+++ Winter-MelonJam-2023/Assets/Podestle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Podestle : Checker
+{
+    protected override void OnActivated()
+    {
+    }
+
+    protected override void OnDeactivated()
+    {
+    }
+
+
+}
